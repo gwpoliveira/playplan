6ddeec42abe5d624adbd6a7992ba19f4ff4c5f39
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-01-23 21:22:43 -0300

    first commit

---
+++ project/views.py
@@ -0,0 +1,10 @@
+from django.http import Http404
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)               
+        return context
