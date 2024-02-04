31124e39385dd1b4f5d6f05bbc4b494b8c58c49b
gwpoliveira <gwpoliveira@gmail.com>
2024-02-04 10:22:22 -0300

    login

--- project/urls.py
+++ project/urls.py
@@ -16,18 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import HomeView, UserProfileView
 from .views import HomeView,ContatoView, BlogView, NoticiaView, OQueETDAH, ApoioView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
     path('', HomeView.as_view(), name='home'),
-    path('admin/', admin.site.urls),
     path('contato/',ContatoView.as_view(), name='contato'),
     path('noticias/', BlogView.as_view(), name='noticias'),
     path('postagem/', NoticiaView.as_view(), name='postagem'),
     path('noticia1/', OQueETDAH.as_view(), name='o_que_e_tdah'),
     path('apoio/', ApoioView.as_view(), name='apoio'),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- project/views.py
+++ project/views.py
@@ -1,6 +1,11 @@
 from django.http import Http404
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth import update_session_auth_hash
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -8,6 +13,20 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)               
         return context
+    
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'registration/profile.html'
+    form_class = UserChangeForm
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        update_session_auth_hash(self.request, self.object)
+        return response
     
 class ContatoView(TemplateView):
     template_name = 'contato/contato.html'
