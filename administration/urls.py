e036212326a9f7d429ed0843346cbcd8e61afef2
willianjonhy05 <willianjohny05@gmail.com>
2024-02-07 17:24:01 -0300

    modified:   administration/urls.py 	modified:   templates/administration/painel.html 	modified:   templates/categorys/criar_categoria.html

--- administration/urls.py
+++ administration/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     ImageListView, ImageCreateView, ImageUpdateView, ImageDeleteView, 
     ContatoCreateView, ContatoListView, ContatoDetailView,
-    BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView, ListaApoiadores, ApoiadorUpdateView, ApoiadorDetailView, ApoiadorCreateView, AtualizarContato, ApoiadorDeleteView
+    BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView, ListaApoiadores, ApoiadorUpdateView, ApoiadorDetailView, ApoiadorCreateView, AtualizarContato, ApoiadorDeleteView, CriarCategoria, ListarCategorias, AtualizarCategoria, DetalharCategoria, ApagarCategoria
 )
 
 urlpatterns = [
@@ -32,6 +32,14 @@ urlpatterns = [
     path('apoiador/detalhar/<int:id>', ApoiadorDetailView.as_view(), name='detalhar_apoiador'),
     path('apoiador/apagar/<int:id>', ApoiadorDeleteView.as_view(), name='apagar_apoiador'),
     path('apoiador/novo/', ApoiadorCreateView.as_view(), name='criar_apoiador'),
+
+
+    #******************* Categorias *******************#
+    path('categorias/', ListarCategorias.as_view(), name='listar_categorias'),
+    path('categorias/nova/', CriarCategoria.as_view(), name='criar_categoria'),
+    path('categorias/detalhar/<int:id>', DetalharCategoria.as_view(), name='detalhar_categoria'),
+    path('categorias/atualizar/<int:id>', AtualizarCategoria.as_view(), name='atualizar_categoria'),
+    path('categorias/apagar/<int:id>', ApagarCategoria.as_view(), name='apagar_categoria'),
 
     
     
