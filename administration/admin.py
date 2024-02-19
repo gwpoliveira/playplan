980bc32e86d1e3c59cdd9e372be07707ed450f64
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-02-19 18:50:08 -0300

    admin da Inscrição

--- administration/admin.py
+++ administration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CarouselImage,Contato, BlogPost, BlogPostImage, Category, Apoiador,Depoimento
+from .models import Inscricao, CarouselImage,Contato, BlogPost, BlogPostImage, Category, Apoiador,Depoimento
 
 admin.site.register(CarouselImage)
 admin.site.register(Contato)
@@ -8,3 +8,4 @@ admin.site.register(BlogPostImage)
 admin.site.register(Category)
 admin.site.register(Apoiador)
 admin.site.register(Depoimento)
+admin.site.register(Inscricao)
