455fd32bceeb86f3d27a02910591af4dd983cdb1
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-02-29 20:50:01 -0300

    Banco Postgree e requirements

--- project/settings.py
+++ project/settings.py
@@ -69,11 +69,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'playplandb',
+        'USER': 'admin',
+        'PASSWORD': 'Samuca10x',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
