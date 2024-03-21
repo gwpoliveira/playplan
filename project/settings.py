f4b7ec905d6b21c3a0da562e588ade405c38305d
willianjonhy05 <willianjohny05@gmail.com>
2024-03-21 18:36:49 -0300

    Atualização pequenina no settings

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'login'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'painel_adm'
 LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
