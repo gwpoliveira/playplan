f4499b1fe840f8424b181a62dd1ecff694383a88
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-07-08 11:33:44 -0300

    ok

---
+++ staticfiles/js/data.js
@@ -0,0 +1,17 @@
+var campoData = document.getElementById('id_date');
+
+campoData.addEventListener('input', function () {
+    this.value = mascaraData(this.value);
+});
+
+
+function mascaraData(data) {
+    data = data.replace(/\D/g, '');
+    if (data.length > 2) {
+        data = data.replace(/^(\d{2})(\d)/g, '$1/$2');
+        if (data.length > 5) {
+            data = data.replace(/^(\d{2})\/(\d{2})(\d{1,4})$/, '$1/$2/$3');
+        }
+    }
+    return data;
+}
